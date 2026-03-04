3cc83a2447addc0c888f86c279a6b57b6387e0a8
Kisang <kisangtree@gmail.com>
2026-03-04 10:09:07 +0900

    inverse dynamics added, position PD control & force PI control

--- pp_base_mujoco/UTILS.py
+++ pp_base_mujoco/UTILS.py
@@ -8,6 +8,10 @@ def get_body_names (model, data):
     body_names = [mujoco.mj_id2name(model,mujoco.mjtObj.mjOBJ_BODY, body_idx) for body_idx in range(model.nbody)]
     return body_names
 
+def get_site_names (model, data):
+    site_names = [mujoco.mj_id2name(model,mujoco.mjtObj.mjOBJ_SITE, site_idx) for site_idx in range(model.nsite)]
+    return site_names
+
 def get_actuator_names (model, data):
     control_names = [mujoco.mj_id2name(model,mujoco.mjtObj.mjOBJ_ACTUATOR,ctrl_idx) for ctrl_idx in range(model.nu)]
     return control_names
@@ -69,3 +73,36 @@ def spec_generate_model(config):
 
     return spec.compile() # returns mujoco model
         
+
+
+""" TRANSFORMS """
+def euler2rmat(roll, pitch, yaw):
+    R_x = np.array([[1, 0, 0],
+                    [0, np.cos(roll), -np.sin(roll)],
+                    [0, np.sin(roll), np.cos(roll)]])
+    
+    R_y = np.array([[np.cos(pitch), 0, np.sin(pitch)],
+                    [0, 1, 0],
+                    [-np.sin(pitch), 0, np.cos(pitch)]])
+    
+    R_z = np.array([[np.cos(yaw), -np.sin(yaw), 0],
+                    [np.sin(yaw), np.cos(yaw), 0],
+                    [0, 0, 1]])
+    
+    R = R_z @ R_y @ R_x
+    return R
+
+def rmat2euler(R):
+    sy = np.sqrt(R[0,0] * R[0,0] +  R[1,0] * R[1,0])
+    singular = sy < 1e-6
+
+    if not singular:
+        roll = np.arctan2(R[2,1] , R[2,2])
+        pitch = np.arctan2(-R[2,0], sy)
+        yaw = np.arctan2(R[1,0], R[0,0])
+    else:
+        roll = np.arctan2(-R[1,2], R[1,1])
+        pitch = np.arctan2(-R[2,0], sy)
+        yaw = 0
+
+    return np.array([roll, pitch, yaw])
